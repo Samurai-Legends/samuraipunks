054eac11f5d20eb2c8eba9353cee565bd7ba2aa7
Thijmen <dev@thijmenmaus.nl>
2023-04-17 08:37:29 +0200

    fix(perf): optimize `<SamuraiPunk/>` component

--- src/components/SamuraiPunk.tsx
+++ src/components/SamuraiPunk.tsx
@@ -4,21 +4,23 @@ import {unitTypes} from '~/constants';
 
 
 export type SamuraiPunkProps = {
-  src: string;
-  selectedUnit: typeof unitTypes[number];
   selectedId: number;
+  selectedUnit: typeof unitTypes[number];
+  src: string;
 }
 
-export const SamuraiPunk = ({src, selectedUnit, selectedId}: SamuraiPunkProps) => {
-  return (
-    <Image
-      style={{
-        boxShadow: '0 0 10px 0 rgba(255,255,255, 0.5)',
-      }}
-      alt={`Image for ${selectedUnit.label} id ${selectedId}`}
-      src={src}
-      width="100%"
-      preview={false}
-    />
-  );
-};
+export const SamuraiPunk = ({
+  selectedId,
+  selectedUnit,
+  src,
+}: SamuraiPunkProps) => (
+  <Image
+    style={{
+      boxShadow: '0 0 10px 0 rgba(255,255,255, 0.5)',
+    }}
+    alt={`Image for ${selectedUnit.label} id ${selectedId}`}
+    src={src}
+    width="100%"
+    preview={false}
+  />
+);
